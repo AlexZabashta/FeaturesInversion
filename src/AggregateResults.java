5043793afa6bf531ccc4136ac12015e0f204e39f
Alexey Zabashta <AlexZabashta@github.com>
2016-08-31 21:36:43 +0300

    add result aggregation

---
+++ src/AggregateResults.java
@@ -0,0 +1,57 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.ifmo.recommendersystem.metafeatures.MetaFeatureExtractor;
+
+import features_inversion.classification.dataset.MetaFeatureExtractorsCollection;
+import misc.FolderUtils;
+
+public class AggregateResults {
+
+    final static String q = File.separator;
+
+    public static void main(String[] args) throws IOException {
+
+        String src = "result" + q + "experiments.TestExp" + q + "init2" + q;
+        String dst = FolderUtils.openOrCreate();
+        List<MetaFeatureExtractor> all = MetaFeatureExtractorsCollection.all();
+        int[] fid = { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 23, 24, 25 };
+        int n = fid.length;
+
+        for (int i = 0; i < n; i++) {
+            int x = fid[i];
+            for (int j = 0; j < i; j++) {
+                int y = fid[j];
+
+                try {
+                    File subFolder = new File(src + x + "_" + y);
+
+                    try (BufferedReader result = new BufferedReader(new FileReader(new File(subFolder + q + "result.txt")))) {
+                        double a = -Double.parseDouble(result.readLine());
+                        double b = -Double.parseDouble(result.readLine());
+
+                        System.out.println(a + " " + b);
+
+                    }
+
+                    System.out.println(subFolder);
+                } catch (IOException exception) {
+                    System.err.println(exception);
+                }
+
+            }
+        }
+
+    }
+}
--- src/experiments/DirectMF.java
+++ src/experiments/DirectMF.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -35,48 +36,67 @@ public class DirectMF {
 
         File dataFolder = new File("data" + File.separator + "carff");
 
-        List<MetaFeatureExtractor> all = MetaFeatureExtractorsCollection.all().subList(0, 53);
+        List<MetaFeatureExtractor> all = MetaFeatureExtractorsCollection.all();
 
-        try (PrintWriter out = new PrintWriter(new File(FolderUtils.clearOrCreate() + "list.txt"))) {
+        int n = 27;
+        boolean[] integer = new boolean[n];
+        Arrays.fill(integer, true);
 
-            for (File arff : dataFolder.listFiles()) {
-                try {
-                    Instances instances = new Instances(new FileReader(arff));
-                    instances.setClassIndex(instances.numAttributes() - 1);
+        for (File arff : dataFolder.listFiles()) {
+            try {
+                Instances instances = new Instances(new FileReader(arff));
+                instances.setClassIndex(instances.numAttributes() - 1);
 
-                    BinDataset dataset = BinDataset.fromInstances(instances);
-                    Instances winst = dataset.WEKAInstances();
-
-                    for (MetaFeatureExtractor mfe : all) {
-                        double value = mfe.extractValue(instances);
-                        out.print(value + " ");
-                        System.out.print(value + " ");
+                for (int i = 0; i < n; i++) {
+                    MetaFeatureExtractor mfe = all.get(i);
+                    double value = mfe.extractValue(instances);
+                    if (Double.isInfinite(value)) {
+                        continue;
                     }
-                    System.out.println();
-
-                    for (MetaFeatureExtractor mfe : all) {
-                        double value = mfe.extractValue(winst);
-                        out.print(value + " ");
-                        System.out.print(value + " ");
+                    if (Double.isNaN(value)) {
+                        continue;
                     }
-                    System.out.println();
-                    System.out.println();
 
-                    // BinDataset dataset = BinDataset.fromInstances(instances);
-                    //
-                    // Point point = new Point(extractor.extract(dataset));
-                    //
-                    // datasets.add(dataset);
-                    // points.add(point);
-
-                } catch (Exception err) {
-                    out.print(err.getMessage());
-                    System.out.println(err.getMessage());
-
-                    System.err.println(arff + " " + err.getMessage());
+                    integer[i] &= Math.round(value) == value;
                 }
-                out.println();
+                //
+                // BinDataset dataset = BinDataset.fromInstances(instances);
+                // Instances winst = dataset.WEKAInstances();
+                //
+                // for (MetaFeatureExtractor mfe : all) {
+                // double value = mfe.extractValue(instances);
+                // out.print(value + " ");
+                // System.out.print(value + " ");
+                // }
+                // System.out.println();
+                //
+                // for (MetaFeatureExtractor mfe : all) {
+                // double value = mfe.extractValue(winst);
+                // out.print(value + " ");
+                // System.out.print(value + " ");
+                // }
+                // System.out.println();
+                // System.out.println();
 
+                // BinDataset dataset = BinDataset.fromInstances(instances);
+                //
+                // Point point = new Point(extractor.extract(dataset));
+                //
+                // datasets.add(dataset);
+                // points.add(point);
+
+            } catch (Exception err) {
+                System.err.println(arff + " " + err.getMessage());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + " " + integer[i] + " " + all.get(i).getName());
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!integer[i]) {
+                System.out.print(i + ", ");
             }
         }
 
